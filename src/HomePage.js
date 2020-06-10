a8bf43f368c84ca691ed99b2c64d6cc089d83f17
Jay Hrycuik <jhrycuik@gmail.com>
2020-06-10 16:00:52 -0600

    initial commit for sendit clothing

---
+++ src/HomePage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export const HomePage = () => (
+    <div className='homePage'>
+        <div className='category-container'>
+            <div className='category-item'>
+                <div className='item'>
+                    <h1 className='item-title'>Hats</h1>
+                    <span className='item-subTitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='category-item'>
+                <div className='item'>
+                    <h1 className='item-title'>Jackets</h1>
+                    <span className='item-subTitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='category-item'>
+                <div className='item'>
+                    <h1 className='item-title'>Shoes</h1>
+                    <span className='item-subTitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='category-item'>
+                <div className='item'>
+                    <h1 className='item-title'>Men</h1>
+                    <span className='item-subTitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='category-item'>
+                <div className='item'>
+                    <h1 className='item-title'>Women</h1>
+                    <span className='item-subTitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
